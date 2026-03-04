de9ce2fc9184ddf8fe02d562a576b880a7536687
puspughimire7-art <puspughimire7@gmail.com>
2026-03-04 10:16:18 +0000

    Add CORS support to weather API

---
+++ src/api/actions.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError } from "axios";
+
+const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+
+export const getWeatherData = async (city: string): Promise<WeatherData> => {
+  return new Promise<WeatherData>((resolve, reject) => {
+    axios
+      .get(`${API_URL}/weather/${city}`)
+      .then((res) => {
+        resolve({
+          city: city,
+          temperature: res.data.temperature,
+          humidity: res.data.humidity,
+          wind: res.data.wind,
+          rain: res.data.rain,
+        });
+      })
+      .catch((error) => {
+        if (axios.isAxiosError(error)) {
+          const axiosError = error as AxiosError;
+          if (axiosError.response?.status === 404) {
+            reject("City not found");
+          } else {
+            // It's a good practice to reject with an Error object
+            reject(axiosError.message);
+          }
+        } else {
+          // Handle non-Axios errors
+          reject("An unknown error occurred");
+        }
+      });
+  });
+};
